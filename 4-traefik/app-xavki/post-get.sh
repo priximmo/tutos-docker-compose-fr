aa431ea58806b115af1893d681022635bbb86e32
xavki <xavki@wordpress.com>
2018-11-26 11:00:13 +0100

    maj 4-traefik

--- 4-traefik/app-xavki/post-get.sh
+++ 4-traefik/app-xavki/post-get.sh
@@ -2,10 +2,10 @@
 
 
 echo "Contenu de la base redis avant POST"
-curl localhost:80
+curl xavki.localhost:80
 
 echo "POST..."
-curl --header "Content-Type: application/json" --request POST --data '{"name":"xavki blog"}' localhost:80
+curl --header "Content-Type: application/json" --request POST --data '{"name":"xavki blog"}' xavki.localhost:80
 
 echo "Contenu de la base redis après POST"
-curl localhost:80
+curl xavki.localhost:80
